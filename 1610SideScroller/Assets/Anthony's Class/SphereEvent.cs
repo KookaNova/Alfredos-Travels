26f0b9f5ffdeab8572f801f34019389ed4231c3e
Connor Home <10873383@my.uvu.edu>
2019-01-30 18:27:18 -0700

    Began Working on the Game
    Created files for the game and a separate scene to work with them in. Also fixed the Particle System so that it only activates when the sphere hits the ground.

---
+++ 1610SideScroller/Assets/Anthony's Class/SphereEvent.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class SphereEvent : MonoBehaviour
+{
+    public UnityEvent Event; 
+    
+    private void OnCollisionEnter2D()
+    {
+        Event.Invoke();
+    }
+}
+
+
+
