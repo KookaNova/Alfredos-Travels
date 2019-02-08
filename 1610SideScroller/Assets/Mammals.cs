32e300aa8c30b16380cb15d24d254754c475b83f
Connor Home <10873383@my.uvu.edu>
2019-02-08 15:01:57 -0700

    Sphere Changes Color and Rotates on Play

--- 1610SideScroller/Assets/Mammals.cs
+++ 1610SideScroller/Assets/Mammals.cs
@@ -2,17 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Fish : Animal
+public class Mammals : MonoBehaviour 
 {
-	public Color FinColor;
+	
 
-	// Use this for initialization
 	void Start() 
 	{
-		print("Hello, World!");
+		
 	}
 	
-	// Update is called once per frame
 	void Update() 
 	{
 		
---
+++ 1610SideScroller/Assets/Sphere.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sphere : MonoBehaviour
+{
+	public string Name;
+	public Color MainColor;
+	void Start()
+	{
+		GetComponent<SpriteRenderer>().color = MainColor;
+	}
+
+	void Update() 
+	{
+		transform.Rotate(0,1,0);
+	}
+}
