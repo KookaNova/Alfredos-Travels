ec743c08a917d6cd2b22f954162321204cd70a61
Connor (School) <10873383@my.uvu.edu>
2019-02-13 11:05:28 -0700

    Wrote In-Class Scriptable Objects

---
+++ 1610SideScroller/Assets/ColorData.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu]
+public class ColorData : ScriptableObject
+{
+    public Color Value;
+}
+//gives Global Color options
+//example: change an entire UI color scheme.
