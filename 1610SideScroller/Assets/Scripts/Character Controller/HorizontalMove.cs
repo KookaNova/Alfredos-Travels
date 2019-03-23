b2b9e76777286901ba0d2f01d8d8fc28a333f2a8
Connor (Home) <10873383@my.uvu.edu>
2019-03-23 01:48:07 -0600

    BrickBreak test scene made

---
+++ 1610SideScroller/Assets/Scripts/Character Controller/HorizontalMove.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HorizontalMove : MonoBehaviour
+{
+    public float Speed;
+    
+    private CharacterController _controller;
+    private Vector3 _position;
+    
+    void Start()
+    {
+        _controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        if (Input.GetAxis("Horizontal") > 0 || Input.GetAxis("Horizontal") < 0)
+        {
+            _position.x = Input.GetAxis("Horizontal") * Speed * Time.deltaTime;
+            _controller.Move(_position);
+        }
+    }
+}
