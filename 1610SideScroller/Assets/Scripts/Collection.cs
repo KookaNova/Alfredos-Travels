04e3420ea21ac42d913ed2341f402b61f9c857a0
Connor (Home) <10873383@my.uvu.edu>
2019-03-27 22:52:39 -0600

    Created ForEach loops, cleaned files

--- 1610SideScroller/Assets/Scripts/Collection.cs
+++ 1610SideScroller/Assets/Scripts/Collection.cs
@@ -1,14 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
 [CreateAssetMenu]
 public class Collection : ScriptableObject
 {
 
-    public List<Object> CollectionList;
+    public List<FloatData> CollectionList;
     
-    public void Collect(Object obj)
+    public void Collect(FloatData obj)
     {
         CollectionList.Add(obj);
+    }
+
+    public void CollectionInfo()
+    {
+        foreach (var obj in CollectionList)
+        {
+            Debug.Log(obj);
+        }
+    }
+
+    public void AmmoInfo()
+    {
+        foreach (var obj in CollectionList)
+        {
+            if (obj.name == "Ammo")
+            {
+                Debug.Log("We have " + obj.Value + " ammo");
+            }
+        }
     }
 }
