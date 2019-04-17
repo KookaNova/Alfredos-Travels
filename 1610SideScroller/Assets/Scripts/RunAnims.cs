5c9c3907b786adfde9c7976c086b11fe7cc16bf6
Connor (School) <10873383@my.uvu.edu>
2019-04-17 12:30:23 -0600

    Destructible environment added

---
+++ 1610SideScroller/Assets/Scripts/RunAnims.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RunAnims : MonoBehaviour
+{
+    public float Speed;
+    private Animator _anims;
+
+    private void Start()
+    {
+        _anims = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        _anims.SetFloat("Speed", Speed);
+    }
+}
+
+    
