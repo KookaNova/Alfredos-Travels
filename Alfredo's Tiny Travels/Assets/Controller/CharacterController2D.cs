f6387ac1d585299c60635b5821d39a9202d1e716
Connor <10873383@my.uvu.edu>
2021-04-02 14:11:57 -0500

    Updated Unity version, new floor sprite, updated Character Controller

--- Alfredo's Tiny Travels/Assets/Controller/CharacterController2D.cs
+++ Alfredo's Tiny Travels/Assets/Controller/CharacterController2D.cs
@@ -3,24 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-[RequireComponent(typeof(CharacterController))]
-public class CharacterMovement : MonoBehaviour, ControlInputs.IPlayerActions
+[RequireComponent(typeof(BoxCollider2D))]
+public class CharacterController2D : MonoBehaviour, ControlInputs.IPlayerActions
 {
     public Animator animator;
+    public GameObject artObj;
     public float speed = 1f, jump = 1f, gravity = 1f, acceleration = 1f, deceleration = .5f;
     private Vector2 _moveVec, _currentVec;
     private ControlInputs _controls;
-    private CharacterController2D _character;
+    private BoxCollider2D boxCollider;
+    private bool isGrounded;
 
     void Awake(){
-        _character = GetComponent<CharacterController>();
+        boxCollider = GetComponent<BoxCollider2D>();
         _controls = new ControlInputs();
         _controls.Player.Move.performed += OnMove;
         _controls.Player.Jump.performed += OnJump;
     } 
 
     private void Update() {
-
+        //tells objects how to move
         if(_moveVec.x != 0){
             _currentVec.x = Mathf.MoveTowards(_currentVec.x, speed * _moveVec.x, acceleration * Time.deltaTime);
 
@@ -28,6 +30,14 @@ public class CharacterMovement : MonoBehaviour, ControlInputs.IPlayerActions
         else{
             _currentVec.x = Mathf.MoveTowards(_currentVec.x, 0, Time.deltaTime);
         }
+        //actually moves object
+        transform.Translate(_currentVec * Time.deltaTime);
+        //code to determine if object is grounded
+        Collider2D[] hits = Physics2D.OverlapBoxAll(transform.position, boxCollider.size, 0);
+
+        //code to add gravity when not grounded
+        
+
         //play walking animation
         if(_moveVec.x != 0){
             animator.SetBool("isWalking", true);
@@ -35,36 +45,17 @@ public class CharacterMovement : MonoBehaviour, ControlInputs.IPlayerActions
         else{
             animator.SetBool("isWalking", false);
         }
-        if(_character.isGrounded == false){
-            _currentVec.y -= gravity * Time.deltaTime;
-        }
         FlipSprite();
 
 
-        transform.Translate(_currentVec * Time.deltaTime);
-
-        Collider2D[] hits = Physics2D.OverlapBoxAll(transform.position, boxCollider.size, 0);
-
-        foreach (Collider2D hit in hits)
-            {
-	            if (hit == boxCollider)
-		        continue;
-
-	            ColliderDistance2D colliderDistance = hit.Distance(boxCollider);
-
-	            if (colliderDistance.isOverlapped)
-	            {
-		            transform.Translate(colliderDistance.pointA - colliderDistance.pointB);
-	            }
-            }
     }
 
     private void FlipSprite(){
         if(_moveVec.x < 0){
-            transform.localScale = new Vector3(-1, 1, 1);
+            artObj.transform.localScale = new Vector3(-1, 1, 1);
         }
         if(_moveVec.x > 0){
-            transform.localScale = new Vector3(1, 1, 1);
+            artObj.transform.localScale = new Vector3(1, 1, 1);
         }
     }
     public void OnMove(InputAction.CallbackContext value)
@@ -74,9 +65,6 @@ public class CharacterMovement : MonoBehaviour, ControlInputs.IPlayerActions
 
     public void OnJump(InputAction.CallbackContext pressed)
     {
-        if(_character.isGrounded){
-            _currentVec.y += jump;
-        }
     }
 
     private void OnEnable() {
